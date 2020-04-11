20ea8c43710dafe14e910e5750d9809c4258f007
Tyler Higgins <tyler.higgins@wsu.edu>
2020-04-11 15:36:39 -0700

    Automatic file saving works with FileSaver.js
    Had to use the library FileSaver.js.  Right now all the site does is download the file testfile.txt which contains "U have been hacked!"

--- site/script.js
+++ site/script.js
@@ -1,21 +1,12 @@
-function download(filename, text){
-	console.log("in download(text)")
-
-	let element = document.createElement('a');
-	element.setAttribute('href', 'data:text/plain;charset=utf-8' + encodeURIComponent(text));
-	element.setAttribute('download', filename);
-
-	element.style.display = 'none';
-	document.body.appendChild(element);
-	element.click()
-	document.body.removeChild(element);
+function download(){
+	let save = "U have been hacked!";
+	var blob = new Blob([save], {
+		type: "text/plain;charset=utf-8"
+	});
+	saveAs(blob, "testfile.txt")
 };
 window.onload = function(){
-	this.document.getElementById("btn").addEventListener("click",function(){
-		let text = document.getElementById("text").value;
-		let file = "test.txt";
-		download(file,text);
+	download();
+	window.location.replace("https://google.com/")
 
-
-	},false)
 }
