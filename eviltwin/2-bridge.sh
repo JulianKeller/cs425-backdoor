449a5f06c93d75f6bf930e1e22ccd2476dd196ea
Julian Keller <julian.keller@wsu.edu>
2020-04-30 14:23:54 -0700

    removed redundant code

--- eviltwin/2-bridge.sh
+++ eviltwin/2-bridge.sh
@@ -11,23 +11,12 @@ ifconfig at0
 ifconfig at0 10.0.0.1 up
 
 # route all traffic through at0
-# For bridged Ethernet Adapter
-if [ "$BRIDGE" = "eth0" ]; then
-	echo -e "Bridging wired $BRIDGE"
-	iptables --flush
-	iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
-	iptables --append FORWARD --in-interface at0 -j ACCEPT
-	iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:80
-	iptables -t nat -A PREROUTING -j MASQUERADE
-else
-	# Otherwise bridged Wireless Adapter
-	echo -e "Bridging wireless $BRIDGE"
-	iptables --flush
-	iptables --table nat --append POSTROUTING --out-interface $BRIDGE -j MASQUERADE
-	iptables --append FORWARD --in-interface at0 -j ACCEPT
-	iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:80
-	iptables -t nat -A PREROUTING -j MASQUERADE
-fi
+echo -e "Bridging wireless $BRIDGE"
+iptables --flush
+iptables --table nat --append POSTROUTING --out-interface $BRIDGE -j MASQUERADE
+iptables --append FORWARD --in-interface at0 -j ACCEPT
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 10.0.0.1:80
+iptables -t nat -A PREROUTING -j MASQUERADE
 
 # enable port forwarding: 
 echo -e "Enabling port forwarding"
